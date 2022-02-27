79b43dfcb0a9fbc4b221b96ab2cdd1899d1a3507
Lucciana <luccianaarroyo@gmail.com>
2022-02-27 16:21:43 -0300

    actualizacion

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,62 +3,40 @@ from AppCoder.views import inicio, servicios, servicios_formulario, profesionale
 from AppCoder.views import ServiciosListView, ServiciosCreateView, ServiciosUpdateView, ServiciosDeleteView, ServiciosDetailView, ProfesionalesListView, ProfesionalesCreateView, ProfesionalesUpdateView, ProfesionalesDeleteView, ProfesionalesDetailView, ConsultaListView, ConsultaCreateView, ConsultaUpdateView, ConsultaDeleteView, ConsultaDetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
-#servicios_delete,
-#profesionales_delete,
-#consulta_delete,
+
 
 urlpatterns = [  
     path('', inicio, name="Inicio"),
+    
+    ##MANERA VIEJA Y LARGA#
+    path('consulta', login_required(consulta), name='Consultas'),
+    path('consultaFormulario', consulta_formulario, name='consulta_formulario'),
+    path('busquedaConsulta', busqueda_consulta, name='busqueda_consulta'),
+    path('buscar', buscar, name='buscar'),
     
     path('busquedaConsulta', busqueda_consulta, name='busqueda_consulta'), # clase 21 a los 1.19 
     path('buscar', buscar, name='buscar'), # clase 21 a los 1.22
     path('nosotros', nosotros, name='nosotros'),
     
+    #MANERA NUEVA Y RESUMIDA#
     path('servicios', ServiciosListView.as_view(), name='Servicios'),
     path('profesores/add', ServiciosCreateView.as_view(), name='servicios_formulario'),
     path('profesores/update/<pk>', ServiciosUpdateView.as_view(), name='servicios_update'),
     path('profesores/delete/<pk>', ServiciosDeleteView.as_view(), name='servicios_delete'),
     path('servicios/view/<pk>', ServiciosDetailView.as_view(), name='servicios_ver'),
     
+    #MANERA NUEVA Y RESUMIDA#
     path('profesionales', ProfesionalesListView.as_view(), name='profesional'),
     path('profesionales/add', ProfesionalesCreateView.as_view(), name='profesionales_formulario'),
     path('profesionales/update/<pk>', ProfesionalesUpdateView.as_view(), name='profesionaless_update'),
     path('profesionales/delete/<pk>', ProfesionalesDeleteView.as_view(), name='profesionales_delete'),
     path('profesionales/view/<pk>', ProfesionalesDetailView.as_view(), name='profesionales_ver'),
     
-    path('consulta', ConsultaListView.as_view(), name='Consulta'),
-    path('profesionales/add', ConsultaCreateView.as_view(), name='consulta_formulario'),
-    path('profesionales/update/<pk>', ConsultaUpdateView.as_view(), name='consulta_update'),
-    path('profesionales/delete/<pk>', ConsultaDeleteView.as_view(), name='consulta_delete'),
-    path('profesionales/view/<pk>', ConsultaDetailView.as_view(), name='consulta_ver'),
+    #NO HACE FALTA#
+    # path('consulta', ConsultaListView.as_view(), name='Consulta'),
+    # path('profesionales/add', ConsultaCreateView.as_view(), name='consulta_formulario'),
+    # path('profesionales/update/<pk>', ConsultaUpdateView.as_view(), name='consulta_update'),
+    # path('profesionales/delete/<pk>', ConsultaDeleteView.as_view(), name='consulta_delete'),
+    # path('profesionales/view/<pk>', ConsultaDetailView.as_view(), name='consulta_ver'),
+    
 ]
-    
-    
-    #LAS NUEVAS URLS TIENE QUE SER ESTAS
-    # path('profesionales/add', ProfesionalesCreateView.as_view(),name='profesionales_add'),
-    # path('profesionales/update', ProfesionalesUpdateView.as_view(),name='profesionales_update'),
-    # path('profesionales/delete', ProfesionalesDeleteView.as_view(),name='profesionales_delete'),
-    # path('login', views.login_request, name = 'Login'),
-    # path('register', views.register, name = 'Register'),
-    # path('logout', LogoutView.as_view(template_name='AppCoder/logout.html'), name = 'Logout'),
-    # path('editarPerfil', views.editarPerfil, name="EditarPerfil"),
-    
-    #path('agregarAvatar', views.agregarAvatar, name="AgregarAvatar"),
-    
-    
-#url viejas#
-
-    #path("servicios", servicios,name="Servicios"),
-    #path('serviciosFormulario', servicios_formulario, name='servicios_formulario'),
-    #path('servicios/delete/<id_servi>', servicios_delete, name='servicios_delete_servicio.id'),
-    #path('servicios/update<id_servi>', servicios_delete, name='servicios_delete'),
-    
-    #path('profesionales', profesionales ,name='Profesionales'),
-    #path('profesionalesFormulario', profesionales_formulario, name='profesionales_formulario'),
-    #path('profesionales/delete/<id_prof>', profesionales_delete, name='profesionales_delete'),
-    #path('profesionales/updatee/<id_prof>', profesionales_delete, name='profesionales_delete'),
-    
-    #path("consultas", consulta,name="Consultas"),
-    #path('consultaFormulario', consulta_formulario, name='consulta_formulario'),
-    #path('consulta/delete/<id_consul', consulta_delete, name='consulta_delete'),
-    #path('consulta/delete/<id_consul>', consulta_delete, name='consulta_delete'),
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse_lazy  
 
-from AppCoder.forms import ServiciosForm, ProfesionalesForm, ConsultaForm, AvatarForm
-from AppCoder.models import Servicios, Profesionales, Consulta, Avatar
+from AppCoder.forms import ConsultaForm, ServiciosForm, ProfesionalesForm, AvatarForm
+from AppCoder.models import Consulta, Servicios, Profesionales, Avatar
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -21,6 +21,11 @@ from django.contrib.auth.decorators import login_required
 
 from django.views.generic.base import TemplateView
 
+#CONSULTA ESTA CODEADO DE LA MANERA VIEJA Y LARGA
+#SERVICIOS Y PROFESIONALES ESTA CODEADO DE LA MANERA NUEVA Y RESUMIDA
+
+#INICIO AVATAR ######################################################################################################
+#AVATAR LARGO#
 def inicio(request):
     #---- AVATAR ROMPE NO RECONOSE OBJETS ---- 
     avatar = Avatar.objects.filter(user=request.user)
@@ -31,96 +36,18 @@ def inicio(request):
         avatar_url = ''
 
     return render(request, 'AppCoder/inicio.html', {'avatar_url': avatar_url})
+
+
+#NUEVA VIEW AVATAR RESUMIDO
+class AvatarView:
+      def get_context_data(self, **kwargs):
+          contexto = super().get_context_data(**kwargs)
+          if self.request.user.is_authenticated:
+              contexto['avatar_url'] = Avatar.objects.filter(user=self.request.user).last().imagen.url
+          return contexto
    
-
-############ SERVICIOS ###########
-
-def servicios(request):
-    return render(request, "AppCoder/servicio.html", {'profesionales': Servicios.objects.all()})
-
-def servicios_formulario (request): #se usan las mismas variables que en el forms 
-    if request.method == 'POST':
-        formulario = ServiciosForm(request.POST)
-        
-        if formulario.is_valid():
-            data = formulario.cleaned_data
-        Servicios.objects.create(nombre =data['nombre'], servicio=data['servicio'], detalleDeServicio=data['detalleDeServicio']) 
-        return redirect('Servicios')    
-    else:
-        formulario = ServiciosForm()
-    return render(request,"AppCoder/consultaFormulario.html", {'formulario': formulario}) 
-
-def servicios_delete (request, id_servi):
-    servicios = Servicios.objects.get(id=id_servi)
-    servicios.delete()
-    
-    return redirect('Servicios')
-
-def servicios_update(request, id_servi):
-    servicios = Servicios.objects.get(id=id_servi)
-    
-    if request.method == 'POST':
-         formulario = ServiciosForm(request.POST)
-
-         if formulario.is_valid():
-             data = formulario.cleaned_data
-             servicios.nombre = data ['nombre']
-             servicios.servicio = data ['servicio']
-             servicios.detalleDeServicio = data ['detalleDeServicio']
-            
-             servicios.save()
-            
-             return redirect ('Servicios')
-    else:
-        formulario = ServiciosForm(model_to_dict(servicios))
-    return render(request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
-
-
-############ PROFESIONALES ############
-
-def profesionales(request):
-    return render(request, "AppCoder/profesional.html", {'profesionales': Profesionales.objects.all()})
-
-def profesionales_formulario (request): #se usan las mismas variables que en el forms 
-    if request.method == 'POST':
-        formulario = ProfesionalesForm(request.POST)
-        
-        if formulario.is_valid():
-            data = formulario.cleaned_data
-        Profesionales.objects.create(nombre =data['nombre'], nombreDeProfesional=data['nombreDeProfesional'], turno=data['turno']) 
-        return redirect('Profesionales')    
-    else:
-        formulario = ProfesionalesForm()
-    return render(request, "AppCoder/consultaFormulario.html", {'formulario': formulario}) 
-
-def profesionales_delete (request, id_prof):
-     profesionales = Profesionales.objects.get(id=id_prof)
-     profesionales.delete()
-    
-     return redirect('Profesionales')
-
-def profesionales_update(request, id_profesional):
-    profesionales = Profesionales.objects.get(id=id_profesional)
-    
-    if request.method == 'POST':
-         formulario = ProfesionalesForm(request.POST)
-
-         if formulario.is_valid():
-             data = formulario.cleaned_data
-             profesionales.nombre = data ['nombre']
-             profesionales.nombreDeProfesional = data ['nombreDeProfesional']
-             profesionales.turno = data ['turno']
-            
-             profesionales.save()
-            
-             return redirect ('Profesionales')
-    else:
-         formulario = ProfesionalesForm(model_to_dict(profesionales))
-    return render (request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
-        
-##### CONSULTA ########
-
-
+   
+##### CONSULTA ######################################################################################################################
 @login_required
 def consulta(request):
  return render(request, "AppCoder/consulta.html", {'consulta': Consulta.objects.all()})
@@ -129,9 +56,7 @@ def consulta(request):
 def consulta_formulario (request): #se usan las mismas variables que en el forms 
     if request.method == 'POST':
         formulario = ConsultaForm(request.POST) # agrega esto y saca nombre post etc a los 37.15 de la clase 21
-        # nombre = request.POST['nombre']
-        # servicio = request.POST['servicio']
-        # mail = request.POST['mail']
+    
         
         if formulario.is_valid():
             data = formulario.cleaned_data
@@ -141,37 +66,62 @@ def consulta_formulario (request): #se usan las mismas variables que en el forms
         formulario = ConsultaForm()
     return render(request, "AppCoder/consultaFormulario.html", {'formulario': formulario}) #agrega {formulario} a los 41.20 clase21
 
-
-def consulta_delete (request, id_consul):
-    consulta = Consulta.objects.get(id=id_consul)
-    consulta.delete()
+#EN CONSULTA NO HACE FLTA EL UPDATE Y DELETE#
+# def consulta_delete (request, id_consul):
+#     consulta = Consulta.objects.get(id=id_consul)
+#     consulta.delete()
     
-    return redirect('Consultas')
+#     return redirect('Consultas')
 
-def consulta_update(request, id_consul):
-    consulta = Consulta.objects.get(id=id_consul)
+# def consulta_update(request, id_consul):
+#     consulta = Consulta.objects.get(id=id_consul)
     
-    if request.method == 'POST':
-         formulario = ConsultaForm(request.POST)
+#     if request.method == 'POST':
+#          formulario = ConsultaForm(request.POST)
 
-         if formulario.is_valid():
-             data = formulario.cleaned_data
-             consulta.nombre = data ['nombre']
-             consulta.servicio = data ['servicio']
-             consulta.mail = data ['mail']
+#          if formulario.is_valid():
+#              data = formulario.cleaned_data
+#              consulta.nombre = data ['nombre']
+#              consulta.servicio = data ['servicio']
+#              consulta.mail = data ['mail']
             
-             consulta.save()
+#              consulta.save()
             
-             return redirect ('Consulta')
-    else:
-        formulario = ConsultaForm(model_to_dict(servicios))
-    return render(request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
+#              return redirect ('Consulta')
+#     else:
+#         formulario = ConsultaForm(model_to_dict(servicios))
+#     return render(request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
 
 
+# NUEVAS VIEWS DE CONSULTAS  NO HACE FALTA #################################################
 
+# class ConsultaListView(LoginRequiredMixin, AvatarView, ListView):
+#      model =  Consulta
+#      template_name = "AppCoder/consulta.html"
+#      context_object_name = 'consulta'
+     
+# class ConsultaDetailView(DetailView):
+#      model=  Consulta
+#      template_name= "AppCoder/consulta_ver.html"
+    
+# class  ConsultaCreateView(AvatarView, CreateView):
+#      model=  Consulta
+#      success_url= reverse_lazy('Consulta')
+#      Fields =['nombre', 'servicio', 'email' ]
+#      template_name = 'AppCoder/consulta_form.html'
+        
+# class  ConsultaUpdateView(UpdateView):
+#      model=  Consulta
+#      success_url= reverse_lazy('Consulta')
+#      Fields =['nombre', 'servicio', 'email' ]
+#      template_name = 'AppCoder/consulta_form.html'
+    
+# class ConsultaDeleteView(DeleteView):
+#      model=  Consulta
+#      success_url= reverse_lazy('consulta')
+#      template_name='AppCoder/profesor_confirm_delete.html'
 
-
-################## BUSQUEDA ############
+################## BUSQUEDA ######################################################################################
 
 #clase 21 a los 1.18 crea esta funcion
 def busqueda_consulta(request):
@@ -190,63 +140,143 @@ def buscar(request):
         return HttpResponse('No es valido')
     
     
+# return HttpResponse(f'Estoy buscando la consulta de: {request.GET["nombre"]}') #el nombre lo saca del template busquedaConsulta en el body esta declarada la variable de busqueda
+
+
+############ SERVICIOS #########################################################################################
+# REEMPLAZADO POR ServiciosListView###########################################
+# def servicios(request):
+#     return render(request, "AppCoder/servicio.html", {'profesionales': Servicios.objects.all()})
+
+
+# REEMPLAZADO POR ServiciosCreateView###########################################
+# def servicios_formulario (request): #se usan las mismas variables que en el forms 
+#     if request.method == 'POST':
+#         formulario = ServiciosForm(request.POST)
+        
+#         if formulario.is_valid():
+#             data = formulario.cleaned_data
+#         Servicios.objects.create(nombre =data['nombre'], servicio=data['servicio'], detalleDeServicio=data['detalleDeServicio']) 
+#         return redirect('Servicios')    
+#     else:
+#         formulario = ServiciosForm()
+#     return render(request,"AppCoder/consultaFormulario.html", {'formulario': formulario}) 
+
+
+# REEMPLAZADO POR ServiciosDeleteView###########################################
+# def servicios_delete (request, id_servi):
+#     servicios = Servicios.objects.get(id=id_servi)
+#     servicios.delete()
     
-    # return HttpResponse(f'Estoy buscando la consulta de: {request.GET["nombre"]}') #el nombre lo saca del template busquedaConsulta en el body esta declarada la variable de busqueda
-
-###################################################################################
-
-#estas tiene que ser la nuevas views, FALTA CREAR SUS HTML#
+#     return redirect('Servicios')
 
 
-class AvatarView:
-     def get_context_data(self, **kwargs):
-         contexto = super().get_context_data(**kwargs)
-         if self.request.user.is_authenticated:
-             contexto['avatar_url'] = Avatar.objects.filter(user=self.request.user).last().imagen.url
-         return contexto
+# REEMPLAZADO POR serviciosUpdateView###########################################
+# def servicios_update(request, id_servi):
+#     servicios = Servicios.objects.get(id=id_servi)
+    
+#     if request.method == 'POST':
+#          formulario = ServiciosForm(request.POST)
 
-# #SERVICIOS#
-class ServiciosListView(LoginRequiredMixin, AvatarView, ListView):
+#          if formulario.is_valid():
+#              data = formulario.cleaned_data
+#              servicios.nombre = data ['nombre']
+#              servicios.servicio = data ['servicio']
+#              servicios.detalleDeServicio = data ['detalleDeServicio']
+            
+#              servicios.save()
+            
+#              return redirect ('Servicios')
+#     else:
+#         formulario = ServiciosForm(model_to_dict(servicios))
+#     return render(request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
+
+# NUEVAS VIEWS DE SERVICIOS####################################################################################
+class ServiciosListView(AvatarView, ListView):
      model =  Servicios
-     template_name = "AppCoder/servicio.html"
-     context_object_name = 'servicio'
-
-class ServiciosDetailView(DetailView):
-     model=  Servicios
-     template_name= "AppCoder/servicios_ver.html"
+     template_name = "AppCoder/servicios.html"
+     context_object_name = 'servicios'
     
-class  ServiciosCreateView(AvatarView, CreateView):
+class  ServiciosCreateView(CreateView):
      model=  Servicios
-     success_url= reverse_lazy('servicio')
+     success_url= reverse_lazy('servicios')
      Fields =['nombre', 'servicio', 'detalleDeServicio' ]
      template_name = 'AppCoder/servicios_form.html'
         
 class  ServiciosUpdateView(UpdateView):
      model=  Servicios
-     success_url= reverse_lazy('servicio')
+     success_url= reverse_lazy('servicios')
      Fields =['nombre', 'servicio', 'detalleDeServicio' ]
      template_name = 'AppCoder/servicios_form.html'
     
 class ServiciosDeleteView(DeleteView):
      model=  Servicios
      success_url= reverse_lazy('servicio')
-     # template_name toma por default 'AppCoder/profesor_confirm_delete.html'
-# #
+     template_name= 'AppCoder/servicios_confirm_delete.html'
+
+class ServiciosDetailView(DetailView):
+     model=  Servicios
+     template_name= "AppCoder/servicios_ver.html"
+
+
+############ PROFESIONALES reemplazado por nuevas views#####################################################################################
+#REEMPLAZADO POR  ListView#
+# def profesionales(request):
+
+#     return render(request, "AppCoder/profesional.html", {'profesionales': Profesionales.objects.all()})
+
+
+#REEMPLAZADO POR  ProfesionalesCreateView####################################
+# def profesionales_formulario (request): #se usan las mismas variables que en el forms 
+
+#     if request.method == 'POST':
+#         formulario = ProfesionalesForm(request.POST)
+        
+#         if formulario.is_valid():
+#             data = formulario.cleaned_data
+#         Profesionales.objects.create(nombre =data['nombre'], nombreDeProfesional=data['nombreDeProfesional'], turno=data['turno']) 
+#         return redirect('Profesionales')    
+#     else:
+#         formulario = ProfesionalesForm()
+#     return render(request, "AppCoder/consultaFormulario.html", {'formulario': formulario}) 
+
+
+# REEMPLAZADO POR ProfesionalesDeleteView###########################################
+# def profesionales_delete (request, id_prof):
+#      profesionales = Profesionales.objects.get(id=id_prof)
+#      profesionales.delete()
     
+#      return redirect('Profesionales')
+
+
+# REEMPLAZADO POR ProfesionalesUpDteView###########################################
+# def profesionales_update(request, id_profesional): 
+#     profesionales = Profesionales.objects.get(id=id_profesional)
     
-# #PROFESIONALES#
-class ProfesionalesListView(LoginRequiredMixin, AvatarView, ListView):
+#     if request.method == 'POST':
+#          formulario = ProfesionalesForm(request.POST)
+
+#          if formulario.is_valid():
+#              data = formulario.cleaned_data
+#              profesionales.nombre = data ['nombre']
+#              profesionales.nombreDeProfesional = data ['nombreDeProfesional']
+#              profesionales.turno = data ['turno']
+            
+#              profesionales.save()
+            
+#              return redirect ('Profesionales')
+#     else:
+#          formulario = ProfesionalesForm(model_to_dict(profesionales))
+#     return render (request, 'AppCoder/consultaFormulario.html', {'formulario': formulario})
+
+
+# NUEVAS VIEWS DE PROFESIONALES#######################################################################
+class ProfesionalesListView(AvatarView, ListView):
      model =  Profesionales
-     template_name = "AppCoder/profesional.html"
+     template_name = "AppCoder/profesionales.html"
      context_object_name = 'profesionales'
      
-
-
-class ProfesionalesDetailView(DetailView):
-     model=  Profesionales
-     template_name= "AppCoder/profesionales_ver.html"
-    
-class  ProfesionalesCreateView(AvatarView, CreateView):
+class  ProfesionalesCreateView(CreateView):
      model=  Profesionales
      success_url= reverse_lazy('profesionales')
      Fields =['nombre', 'nombreDeProfesional', 'turno' ]
@@ -261,41 +291,13 @@ class  ProfesionalesUpdateView(UpdateView):
 class ProfesionalesDeleteView(DeleteView):
      model=  Profesionales
      success_url= reverse_lazy('profesionales')
-     # template_name toma por default 'AppCoder/profesor_confirm_delete.html'
-    
-    
-# #CONSULTAS#
+     template_name= 'AppCoder/profesionales_confirm_delete.html'
 
-class ConsultaListView(LoginRequiredMixin, AvatarView, ListView):
-     model =  Consulta
-     template_name = "AppCoder/consulta.html"
-     context_object_name = 'consulta'
-     
-
-
-class ConsultaDetailView(DetailView):
-     model=  Consulta
-     template_name= "AppCoder/consulta_ver.html"
+class ProfesionalesDetailView(DetailView):
+     model=  Profesionales
+     template_name= "AppCoder/profesionales_ver.html"   
+###################################################################################
     
-class  ConsultaCreateView(AvatarView, CreateView):
-     model=  Consulta
-     success_url= reverse_lazy('Consulta')
-     Fields =['nombre', 'servicio', 'email' ]
-     template_name = 'AppCoder/consulta_form.html'
-        
-class  ConsultaUpdateView(UpdateView):
-     model=  Consulta
-     success_url= reverse_lazy('Consulta')
-     Fields =['nombre', 'servicio', 'email' ]
-     template_name = 'AppCoder/consulta_form.html'
-    
-class ConsultaDeleteView(DeleteView):
-     model=  Consulta
-     success_url= reverse_lazy('consulta')
-     # template_name toma por default 'AppCoder/profesor_confirm_delete.html'
-    
-    
-
 def nosotros (request):
     return render(request, "AppCoder/nosotros.html")
 
